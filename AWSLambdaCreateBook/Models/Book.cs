f4b8d2dc64321e5a2477a1833254e423202882d9
Benjamin Correa <iambenjamin.developer@gmail.com>
2025-08-19 11:08:26 -0300

    add function

---
+++ AWSLambdaCreateBook/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+
+namespace AWSLambdaCreateBook.Models
+{
+    [DynamoDBTable("books")]
+    public class Book
+    {
+        [DynamoDBHashKey]
+        public string Id { get; set; } = default!;
+
+        public string Title { get; set; } = default!;
+        public string Author { get; set; } = default!;
+        // Agrega más propiedades según tu estructura JSON
+    }
+}
